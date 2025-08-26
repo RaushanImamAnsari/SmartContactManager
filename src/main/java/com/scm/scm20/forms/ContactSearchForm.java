ba82d7d1f3f324177e18d23e3f0c8ead68883b39
RaushanImamAnsari <raushanimamansari@gmail.com>
2025-08-27 01:08:40 +0530

    Initial commit

---
+++ src/main/java/com/scm/scm20/forms/ContactSearchForm.java
@@ -0,0 +1,14 @@
+
+package com.scm.scm20.forms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContactSearchForm {
+
+    private String field;
+    private String value;
+
+}
