81280df8d7cd715245c02f7d03f1b197bfb26100
RaushanImamAnsari <raushanimamansari@gmail.com>
2026-01-11 20:00:42 +0530

    Repo update

--- src/main/java/com/scm/scm20/repositories/ContactRepo.java
+++ src/main/java/com/scm/scm20/repositories/ContactRepo.java
@@ -28,4 +28,5 @@ public interface ContactRepo extends JpaRepository<Contact, String> {
 
     Page<Contact> findByUserAndPhoneNumberContaining(User user, String phonekeyword, Pageable pageable);
 
+
 }
