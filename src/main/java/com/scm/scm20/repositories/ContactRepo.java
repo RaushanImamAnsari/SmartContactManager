b1f8725dfb02f0749d96441a67ce3f028d895f39
RaushanImamAnsari <raushanimamansari@gmail.com>
2026-01-15 18:42:46 +0530

    Repo update

--- src/main/java/com/scm/scm20/repositories/ContactRepo.java
+++ src/main/java/com/scm/scm20/repositories/ContactRepo.java
@@ -18,10 +18,6 @@ public interface ContactRepo extends JpaRepository<Contact, String> {
     // custom finder method
     Page<Contact> findByUser(User user, Pageable pageable);
 
-
-
-
-
     // custom query method
     @Query("SELECT c FROM Contact c WHERE c.user.id = :userId")
     List<Contact> findByUserId(@Param("userId") String userId);
